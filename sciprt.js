9076bf4719aa6bc26faa38086ee2350ccd21ee58
JuanMT29 <juanmartintoffe@gmail.com>
2023-11-02 20:12:17 -0300

    App Subida

---
+++ sciprt.js
@@ -0,0 +1,88 @@
+const result = document.querySelector('.result');
+const form = document.querySelector('.get-weather');
+const nameCity = document.querySelector('#city');
+const nameCountry = document.querySelector('#country');
+
+form.addEventListener('submit',(e) =>{
+    e.preventDefault();
+    if(nameCity.value === '' || nameCountry === ''){
+        showError('Ambos campos son obligatorios...');
+        return;
+    }
+
+    callAPI(nameCity.value, nameCountry.value);
+    //console.log(nameCity.value);
+    //console.log(nameCountry.value);
+} )
+
+
+function callAPI(city, country){
+    const apiId = '2a251ecd9891aab94c213a7da2d21dcf';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiId}`;
+
+    fetch(url)
+        .then(data => {
+            return data.json();
+        })
+        .then(dataJSON => {
+            if(dataJSON.cod === '404'){
+                showWeather('Ciudad no encontrada...')
+            }else{
+                limpiar();
+                showWeather(dataJSON);
+            }
+            console.log(dataJSON)
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+}
+
+function showWeather(data){
+    const {name, main:{temp, temp_min, temp_max}, weather:[arr] } = data;
+
+    const degress = kelvinToCentigrade(temp);
+    const min = kelvinToCentigrade(temp_min);
+    const max = kelvinToCentigrade(temp_max);
+
+
+
+    const content = document.createElement('div');
+    content.innerHTML = `
+        <h5>Clima en: ${name}</h5>
+        <img src="https://openweathermap.org/img/wn/${arr.icon}@2x.png" alt="icon">
+        <h2>${degress}°C</h2>
+        <p>Max:${max}</p>
+        <p>Min:${min} </p>
+    `;
+
+    result.appendChild(content);
+
+    // console.log(name);
+    // console.log(temp);
+    // console.log(temp_max);
+    // console.log(temp_min);
+    // console.log(arr.icon);
+
+
+}
+
+function showError(message){
+    console.log(message);
+    const alert = document.createElement('p');
+    alert.classList.add('alert-message');
+    alert.innerHTML = message;
+
+    form.appendChild(alert);
+    setTimeout(() => {
+        alert.remove();
+    } , 1500);
+}
+
+function kelvinToCentigrade(temp){
+    return parseInt(temp - 273.15);
+}
+
+function limpiar(){
+    result.innerHTML = '';
+}
